b00c2083957a37b79786964509fa029f25fd022f
huan.hu@tuya.com <huan.hu@tuya.com>
2021-02-25 15:24:51 +0800

    移除安全图片、appkey和secret 适配只读dp展示

--- Example/TuyaSmartPlanter-ObjC/AppKey.h
+++ Example/TuyaSmartPlanter-ObjC/AppKey.h
@@ -7,7 +7,7 @@
 #ifndef AppKey_h
 #define AppKey_h
 
-#define APP_KEY @"ytg7uvgth48m43rp55d8"
-#define APP_SECRET_KEY @"4f95uvqjp94acmt3txh9shfrgrp4ehwr"
+#define APP_KEY @"*********************"
+#define APP_SECRET_KEY @"*********************"
 
 #endif /* AppKey_h */
